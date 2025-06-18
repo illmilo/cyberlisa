740cfabf4c3eaeab5500827041aaa22776a7910c
illmilo <illfqm@gmail.com>
2025-06-18 23:16:06 +0300

    V0.2.2
    Added checkbox dropdown for Users/Roles. Added database integration for Users.

--- users/views.py
+++ users/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from .models import Roles
+
 # Create your views here.
 def users(request):
-    return render(request, 'users/users.html')
+    roles = Roles.objects.all()
+    return render(request, 'users/users.html', {'roles': roles})
